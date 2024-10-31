16a666f9cc82ab9eef6a3ab9098e6f680674da70
izzycommits <izzy24877@gmail.com>
2024-10-31 17:59:15 -0400

    create model, register blueprint, and create get api

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,20 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from typing import Optional
+import datetime
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[Optional[datetime]] 
+    is_complete: Mapped[bool] = mapped_column(default=False)
+
+    def to_dict(self): 
+        return dict(
+            id=self.id,
+            title=self.title, 
+            description=self.description,
+            completed_at=self.completed_at,
+            is_complete=self.is_complete
+        )
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,35 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response, request, Response
+from app.models.task import Task
+from app.db import db
+
+task_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
+
+@task_bp.post("")
+def create_task(): 
+    request_body = request.get_json()
+    title = request_body["title"]
+    description = request_body["description"]
+    is_complete = request_body["is_complete"]
+
+    new_task = Task(title=title, description=description, is_complete=is_complete)
+    db.session.add(new_task)
+    db.session.commit()
+
+    response = new_task.to_dict()
+    return response, 201
+
+
+@task_bp.get("")
+def get_all_tasks(): 
+    query = db.select(Task)
+    title_param = request.args.get("title")
+    if title_param:
+        query = query.where(Task.title.ilike(f"%{title_param}%"))
+    
+    query=query.order_by(Task.id)
+    tasks = db.session.scalars(query)
+
+    tasks_response =[task.to_dict() for task in tasks]
+
+
+    return tasks_response
