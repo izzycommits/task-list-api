19fff6d48e8c702b8a75acfcbc9812cfa0293f0b
izzycommits <izzy24877@gmail.com>
2024-11-05 04:05:15 -0500

    Update task and goal model

--- app/models/task.py
+++ app/models/task.py
@@ -1,14 +1,20 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
 from ..db import db
 from typing import Optional
 from datetime import datetime
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+  from .goal import Goal
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str] 
     completed_at: Mapped[Optional[datetime]] 
-
+    goal_id: Mapped[Optional[int]] = mapped_column(ForeignKey("goal.id"))
+    goal: Mapped[Optional["Goal"]] = relationship(back_populates="goals")
 
     def to_dict(self):
         is_complete_flag = False 
