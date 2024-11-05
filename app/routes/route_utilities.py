df3987a605e97cad163f7b124355fad9c4b11dcb
izzycommits <izzy24877@gmail.com>
2024-11-05 03:24:43 -0500

    Add route/utilities directory. Move and refactor validate model to new directory.

---
+++ app/routes/route_utilities.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, abort, make_response, request, Response
+from ..db import db
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except ValueError: 
+        abort(make_response({"message":f"{cls.__name__} {model_id} is invalid"}, 400))
+
+    query = db.select(cls).where(cls.id == model_id) 
+    model = db.session.scalar(query)
+
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} is not found"}, 404))
+
+    return model
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, abort, make_response, request, Response
 from app.models.task import Task
 from app.db import db
 from datetime import date
+from .route_utilities import validate_model
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 
@@ -47,13 +48,13 @@ def get_all_tasks():
 
 @tasks_bp.get("/<task_id>")
 def get_single_task(task_id):
-    task = validate_task(task_id)
+    task = validate_model(Task, task_id)
 
     return {"task":task.to_dict()}
 
 @tasks_bp.put("/<task_id>")
 def update_task(task_id):
-    task = validate_task(task_id)
+    task = validate_model(Task, task_id)
     request_body = request.get_json()
 
     task.title = request_body["title"]
@@ -66,7 +67,7 @@ def update_task(task_id):
 
 @tasks_bp.patch("/<task_id>/mark_complete")
 def update_task_complete(task_id):
-    task = validate_task(task_id)
+    task = validate_model(Task, task_id)
     # request_body = request.get_json()
 
     task.completed_at = date.today()
@@ -78,7 +79,7 @@ def update_task_complete(task_id):
 
 @tasks_bp.patch("/<task_id>/mark_incomplete")
 def update_task_incomplete(task_id):
-    task = validate_task(task_id)
+    task = validate_model(Task, task_id)
 
     task.completed_at = None
     db.session.add(task)
@@ -88,7 +89,7 @@ def update_task_incomplete(task_id):
     return response
 @tasks_bp.delete("/<task_id>")
 def delete_task(task_id):
-    task = validate_task(task_id)
+    task = validate_model(Task, task_id)
     
     db.session.delete(task)
     db.session.commit()
@@ -97,16 +98,3 @@ def delete_task(task_id):
     return response
     # return Response(status = 204, mimetype = "application/json")
 
-def validate_task(task_id):
-    try:
-        task_id = int(task_id)
-    except ValueError: 
-        abort(make_response({"message":f"Task id {task_id} is invalid"}, 400))
-
-    query = db.select(Task).where(Task.id == task_id)
-    task = db.session.scalar(query)
-
-    if not task:
-        abort(make_response({"message":f"Task id {task_id} is not found"}, 404))
-
-    return task
